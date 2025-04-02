dc02d22731704c2d66cd6ad6e089755ab5b074fa
Siddhesh2377 <darkwildhacker@gmail.com>
2025-04-02 19:39:01 +0530

    init

--- app/src/main/java/com/dark/project_widget_hehe/MainActivity.kt
+++ app/src/main/java/com/dark/project_widget_hehe/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.dark.project_widget_hehe.ui.theme.ProjectWidgetHeheTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectWidgetHeheTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Box(Modifier.fillMaxSize()) {
+                    Text(
+                        "Nice Widget", Modifier.align(Alignment.Center), fontFamily = FontFamily(
+                            Font(R.font.sansita_bold)
+                        ),
+                        fontSize = 34.sp
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectWidgetHeheTheme {
-        Greeting("Android")
     }
 }
